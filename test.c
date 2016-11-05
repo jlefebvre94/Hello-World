6ccd546cf8a1bd1f3e08ba4ad73fe7f8b26acfaa
jlefebvre94 <jlefebvre94@hotmail.com>
2016-11-05 15:28:10 +0100

    ajout fonction afficheNombre

--- test.c
+++ test.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(int argnb, char * argv[]) {
+<<<<<<< Updated upstream
 	printf("Ceci est un TEST");
+=======
+	int testAfficheNombre(int nunber);
+	printf("Ceci est un test");
+>>>>>>> Stashed changes
+	testAfficheNombre(5);
 	return 0;
 }
+
+int testAfficheNombre(int nunber) {
+		printf("le nombre est %d. \n", number);
+}	
